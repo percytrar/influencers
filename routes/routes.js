a896333487b939e47714485fce0e291f882a55f2
percy <prashant.trar97@yahoo.in>
2019-06-22 02:31:31 +0530

    Second Commit

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const influencer = require('../controllers/influencers');
 
+router.use('/', (req,res,next)=>{
+    next();
+})
 router.get('/influencers', influencer.getInfluencers);
 
 module.exports = router;
